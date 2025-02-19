646c08ea757ede0a4a1cf1acccf36ffb4926099e
Leonardo Buzzoni <leonardobuzzonijaime@gmail.com>
2025-02-19 23:52:25 +0000

    Teste

---
+++ aula_02/Espetaculo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Espetaculo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Custo de Produção: ");
+        float custoProducao = entrada.nextFloat();
+        System.out.print("Valor do Ingresso: ");
+        float valorIngresso = entrada.nextFloat();
+
+        int qtdeConvites = (int) Math.ceil(custoProducao / valorIngresso);
+        int qtdeConvites23 = (int) Math.ceil(custoProducao * 1.23 / valorIngresso);
+        System.out.printf("Quantidade de Convites = %d\n", qtdeConvites);
+        System.out.printf("Lucro de 23%%: %d\n", qtdeConvites23);
+    }
+}
